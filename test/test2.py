898c0939049e4f3470979478679040b3004363b7
liangz77 <>
2024-05-14 09:01:33 +0800

    开发中。。。

--- test/test2.py
+++ test/test2.py
@@ -3,32 +3,66 @@ sys.path.append(os.getcwd())
 from mysql_db.mysql_tool import *
 
 
-sql = 'select * from zyx.sz000001'
-ret = query_sql(sql, logging=False)
-a=1
+#sql = 'select * from zyx.sz000001'
+#ret = query_sql(sql, logging=False)
 
-class stock:
+class Chunk:
+    def __init__(self):
+        self.timev = []
+        self.openv = []
+        self.closev = []
+        self.low = []
+        self.high = []
+        self.volume = []
+
+    def squeeze(self) -> 'Chunk':
+        pass
+
+    def append(self, timev, price, volume):
+        self.timev.append(timev)
+        self.closev.append(price)
+        self.volume.append(volume)
+
+    def append_full(self, time, open, close, low, high, volume):
+        pass
+
+
+class StockInfo:
+    start_date=None
+    end_date=None
+    day=Chunk()
+    min5={}
+
+
+class Stock:
     def __init__(self, code) -> None:
-        sql = 'select * from zyx.sz000001'
+        sql = f'select * from zyx.{code}'
         self.min5 = query_sql(sql, logging=False)
-        self.data = self._arange_data()
+        self.info = self._arange_data()
 
 
     def _arange_data(self):
-        data = {'start_date':None,
-                'end_date':None,
-                'data':{'day':{},
-                        'min5':{}}
-                }
+        info = StockInfo()
         for d in self.min5:
             dt, price, volume = d['datetime'], d['price'], d['volume']
             date1, time1 = dt.split(' ')
-            if data.start_date is None:
-                data.start_date = date1
-            data.end_date = date1
-            
+            if info.start_date is None:
+                info.start_date = date1
+            info.end_date = date1
+            if date1 not in info.min5:
+                info.min5[date1] = Chunk()
+            info.min5[date1].append(time1, price, volume)
+        for min5 in info.min5:
+            asdfasdfsadf
+        self.info = info
+        a=1
 
 
     def _calc_daily(self):
         for d in self.min5:
             pass
+
+
+if __name__ == '__main__':
+    stock = Stock('sz000001')
+    a=1
